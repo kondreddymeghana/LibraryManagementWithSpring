4be3fad53802ae557ff066c07cd8814cb9d50b89
kondreddymeghana <meghanakondreddy18@gmail.com>
2025-07-07 18:42:39 +0530

    Create App.java

---
+++ src/main/java/com/library/App.java
@@ -0,0 +1,7 @@
+package com.library;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Maven project with Spring dependencies created successfully!");
+    }
+}
